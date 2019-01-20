fb50fab181a551a5245eac70b8e5890365124621
M.mahdee <mahdee.dark@gmail.com>
2019-01-20 10:59:49 +0700

    update

---
+++ js/ui.js
@@ -0,0 +1,7 @@
+$("#date").datepicker({
+    dateFormat: 'วัน/เดือน/yy', 
+    minDate: '-100y', 
+    maxDate: 'true',
+    changeMonth:true,
+    changeYear:true
+});
---
+++ test1.php
@@ -0,0 +1,16 @@
+<html leng="en">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+</head>
+<body>
+
+    <label> มาแว้วววววววววววววว</label>
+    <input type="text" id="date" size="8" >
+    
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/ui.js"></script>
+</body>
+</html>
